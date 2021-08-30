236599110c939f74b6f533df273f4ac72fff51f6
Bognaum <bognaum@gmail.com>
2021-08-30 11:20:52 +0300

    Converted spaces to tabs.

--- CSS/watch.js
+++ CSS/watch.js
@@ -4,21 +4,21 @@ import url$  from "url";
 
 import {fileURLToPath}  from "url";
 const 
-  __filename = fileURLToPath(import.meta.url),
-  __dirname  = path$.dirname(__filename);
+	__filename = fileURLToPath(import.meta.url),
+	__dirname  = path$.dirname(__filename);
 
 nodemon$({
-  script: path$.resolve(__dirname, 'compile.js'),
-  ext: 'scss'
+	script: path$.resolve(__dirname, 'compile.js'),
+	ext: 'scss'
 });
 
 nodemon$.on('start', function () {
-  console.log('\n -', getTime(), '\n    Started:', __dirname, ' \n');
+	console.log('\n -', getTime(), '\n    Started:', __dirname, ' \n');
 }).on('quit', function () {
-  console.log('\n -', getTime(), '\n    Quit:', __dirname, ' \n');
-  process.exit();
+	console.log('\n -', getTime(), '\n    Quit:', __dirname, ' \n');
+	process.exit();
 }).on('restart', function (files) {
-  console.log('\n -', getTime(), '\n    Restarted due to: ', files, '\n');
+	console.log('\n -', getTime(), '\n    Restarted due to: ', files, '\n');
 });
 
 function getTime() {
