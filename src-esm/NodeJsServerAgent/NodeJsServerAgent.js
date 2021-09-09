04a3e9118237096f66c54915a4c68c78e80d0b1d
Bognaum <bognaum@gmail.com>
2021-09-09 14:22:40 +0300

    The new principle of port selection. It needs improvement.

--- src-esm/NodeJsServerAgent/NodeJsServerAgent.js
+++ src-esm/NodeJsServerAgent/NodeJsServerAgent.js
@@ -20,6 +20,13 @@ export default class ServerAgent {
 
 }
 
+const servers = [];
+servers.log = function(theServer) {
+	// console.log("servers length:", this.length);
+	for (const [i, serv] of this.entries()) 
+		console.log(i, serv, serv.address()?.port, theServer === serv? "<": "");
+}
+
 async function startServer (o) {
 	const 
 		http$ = await import("http"),
@@ -39,8 +46,7 @@ async function startServer (o) {
 		res.setHeader("Access-Control-Allow-Origin", "*");
 
 		if (req.url == "/port-test") {
-			// console.log("port-test");
-			res.end("ok");
+			res.end("Port test is OK.");
 		} else if (req.url == "/parse") {
 			res.setHeader("Content-Type", 'text/plain; Charset="UTF-8"');
 			res.setHeader("Cash-Control", "no-store");
@@ -73,7 +79,7 @@ async function startServer (o) {
 					agentPage,
 					{
 						name: o.name,
-						theUrl: o.addresData?.host,
+						theUrl: o.netOpts?.host,
 						__basename: path$.basename(__filename),
 					}
 				)
@@ -96,19 +102,20 @@ async function startServer (o) {
 			}
 		}
 	});
+
+	servers.push(server);
+
+	server.on("error", function(err) {
+		// console.log(`server.on error - err >>`, err);
+		// servers.log(server);
+		if (err.code === "EADDRINUSE") {
+			// console.log(`err.name === "EADDRINUSE"`);
+			listenPort(server, o.hostname, portGenerator, o);
+		}
+	});
 	// server.listen(...o.portHost);
-	const addresData = await takeEmptyPort(server, o.hostname, o.ports);
-	if (addresData) {
-		o.addresData = addresData;
-		console.log("Connection with", `'${o.name}'`, ":", `http://${addresData.host}`);
-	} else {
-		console.log(
-			"Can't use an address from range:", 
-			"\n hostname:", o.hostname, 
-			"\n ports   :", o.ports,
-			"\n to input:", `'${o.name}'`
-		);
-	}
+	const portGenerator = getPortGen(o.ports);
+	listenPort(server, o.hostname, portGenerator, o);
 
 	// setInterval(() => {}, 60000);
 }
@@ -132,44 +139,20 @@ function getMIME(fileName) {
 	return "text/plain";
 }
 
+function listenPort(server, hostame, portGenerator, o) {
+	const port = portGenerator.next().value;
+	if (port)
+		server.listen(port, hostame, () => {
+			console.log("Connection with", `'${o.name}'`, ":", `http://${hostame}:${port}`);
+		});
+}
 
-async function takeEmptyPort(server, hostname, portRanges) {
-	const rangedArr = portRanges.split(",");
-	for (let range of rangedArr) {
-		const res = await takeEmptyPortFromRange(server, hostname, range);
-		if (res) 
-			return res;
+function * getPortGen(portRanges) {
+	const portRangesList = portRanges.split(",");
+	for (const portRange of portRangesList) {
+		const [firstPort, lastPort] = portRange.split("-").map((v) => parseInt(v));
+		for (let port = firstPort; port < lastPort; port ++) {
+			yield port;
+		}
 	}
-}
-
-async function takeEmptyPortFromRange(server, hostname, portRange) {
-	const [firstPort, lastPort] = portRange.split("-").map((v) => parseInt(v));
-	for (let p = firstPort; p < lastPort; p ++) 
-		try {
-			await testingAPort(server, hostname, p);
-			return {
-				host: `${hostname}:${p}`, 
-				hostname,
-				port: p
-			};
-		} catch (err) {}
-		// } catch (err) {console.log(`err >>`, err);}
-	return false;
-}
-
-async function testingAPort(server, hostname, port) {
-	const http$ = await import("http");
-	const opts = {
-		port, 
-		host: hostname,
-		path: "/port-test",
-	};
-	await new Promise((rsl, rj) => {
-		server.once("error", rj);
-		server.once("request", rsl);
-		server.listen(opts, () => {});
-		http$.get(opts, () => {})
-		// server.listen(opts, (... args) => {console.log(`server.listen args >>`, args);});
-		// http$.get(opts, (... args) => {console.log(`http$.get args >>`, args);})
-	});
 }
